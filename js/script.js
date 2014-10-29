a5a45c76a7feb8ad3b02717d4546cb81f37020a8
chrisjefferies <github@cjefferies.com>
2014-10-28 22:18:10 -0400

    Divided Item engine
    Separated item engine into a separate javascript file for ease of
    development. Plan to later recompile and minify before launch. Some
    updated styles.

--- js/script.js
+++ js/script.js
@@ -87,6 +87,20 @@ function shield(paramone, paramtwo, paramthr, paramfou, paramfiv, paramsix, para
 	this.donning = paramele;
 }
 
+function customWeapon(paramone, paramtwo, paramthr, paramfou, paramfiv, paramsix, paramsev, parameig, paramnin, paramten, paramele) {
+	this.wName = paramone;
+	this.iWeight = paramtwo;
+	this.damage = paramthr;
+	this.critical = paramfou;
+	this.range = paramfiv;
+	this.wType = paramsix;
+	this.wSize = paramsev;
+	this.wReach = parameig;
+	this.hardness = paramnin;
+	this.hitPoints = paramten;
+	this.wSaves = paramele;
+}
+
 window.itemPurchaseNo = 0;
 window.numOfFeats = 0;
 window.blankCounter = 0;
@@ -95,6 +109,7 @@ window.blankKnowledgeCounter = 0;
 window.blankPerformCounter = 0;
 window.blankProfessionCounter = 0;
 window.startingFeats = 1;
+window.customItemNo = 0;
 
 
 
@@ -278,21 +293,6 @@ var navigate = function(idCalled) {
 		break;
 	}
 }
-
-
-var equipSubMenu = function(whichSubMenu) {
-	var allSubMenus = ["simpweap", "martweap", "exotweap", "armor", "gear", "substances", "tools"] ;
-	for ( i = 0 ; i < allSubMenus.length ; i++ ) {
-		if (allSubMenus[i] === whichSubMenu ) {
-			document.getElementById("equip-" + whichSubMenu).style.display = "block" ;
-		} else {
-			document.getElementById("equip-" + allSubMenus[i] ).style.display = "none" ;
-		}
-	}
-	window.scrollTo(0,0);
-}
- 
-
 
 var rollHeroic = function() {	
 	var statValue = [0, 0, 0, 0, 0, 0];
@@ -1480,340 +1480,6 @@ var featUpdateSkillTwin = function(featShortCode) {
 	}
 }
 
-
-
-
-
-
-
-//	Treat	PLATINUM		=	1000
-// 			GOLD 		= 	100
-// 			SILVER 		= 	10
-//			COPPER		=	1
-//	
-
-var calculateGold = function(mxFactor, fMonks) {	
-	var startingGold = 0;
-	for ( i = 0 ; i < mxFactor ; i++ ) {	// Roll a d4 [mxFactor] times.
-		startingGold += ( Math.floor( Math.random() * 4 + 1) * fMonks ) * 100; // Calculate Number of Coppers in Possession. 
-	}
-	document.getElementById("copper-remaining").innerHTML = startingGold; // Display.
-	document.getElementById("roll-starting-gold").style.display = "none" ; // Remove button to prevent repeats. 
-	document.getElementById("inventory").style.display = "block";
-	document.getElementById("equip-variable-buffer").style.display = "block" ;
-}
-
-
-
-
-var rollStartingGold = function (input) {
-
-	if (input === "first") {
-		switch (window.selClass) {
-			case "DRUID":
-				calculateGold(2, 10);
-			break;
-			case "SORCERER":
-			case "WIZARD":
-				calculateGold(3, 10);
-			break;
-			case "BARD":
-			case "BARBARIAN":
-				calculateGold(4, 10);
-			break;
-			case "CLERIC":
-			case "ROGUE":
-				calculateGold(5, 10);
-			break;
-			case "FIGHTER":
-			case "PALADIN":
-			case "RANGER":
-				calculateGold(6, 10);
-			break;
-			case "MONK":
-				calculateGold(5, 1);
-			break;
-			case "NewBlankClass":
-				// A placeholder for future class additions. 
-			break;
-		}
-	}
-	if (input === "leveled") {
-		/*
-		var gold = 0;
-		for ( i = 0 ; i < window.levelAdvance ; i++ ) {
-			gold += ( 9 * i ) * 10;
-		} */
-
-		switch (Number(window.levelAdvance) ) {
-			case 2:
-				var gold = 90000;
-			break;
-			case 3:
-				var gold = 270000;
-			break;
-			case 4:
-				var gold = 540000;
-			break;
-			case 5:
-				var gold = 900000;
-			break;
-			case 6:
-				var gold = 1300000;
-			break;
-			case 7:
-				var gold = 1900000;
-			break;
-			case 8:
-				var gold = 2700000;
-			break;
-		}
-
-
-		document.getElementById("copper-remaining").innerHTML = gold;
-		document.getElementById("roll-starting-gold").style.display = "none" ; // Remove button to prevent repeats. 
-		document.getElementById("inventory").style.display = "block";
-		document.getElementById("equip-variable-buffer").style.display = "block" ;
-	}
-	if (input === "input") {
-		document.getElementById("copper-remaining").innerHTML = document.getElementById("gold-input").value;
-		document.getElementById("roll-starting-gold").style.display = "none" ; // Remove button to prevent repeats. 
-		document.getElementById("inventory").style.display = "block";
-		document.getElementById("equip-variable-buffer").style.display = "block" ;
-	}
-}
-
-var invDisplay = function() {
-	if (window.itemPurchaseNo < 26 ) {
-		document.getElementById("equip-variable-buffer").style.height = ( 300 - (window.itemPurchaseNo * 11 ) ).toString() + "px" ;
-		if (window.itemPurchaseNo === 25 ) {
-			document.getElementById("inventory-2").style.display = "block" ;
-		}
-	}
-	if (26 < window.itemPurchaseNo < 50 ) {
-		document.getElementById("equip-variable-buffer-2").style.height = ( 600 - (window.itemPurchaseNo * 11 ) ).toString() + "px" ;
-	}
-
-	if (window.itemPurchaseNo > 60 ) {
-		alert("Capacity Reached");
-	}
-}
-
-
-var buyItem = function(itemName, itemCost, itemWeight) {
-
-	// alert("window item item-purchase-no :" + window.itemPurchaseNo);
-	
-	var goldRemaining = Number( document.getElementById("copper-remaining").innerHTML ) ;
-
-	if (itemCost <= goldRemaining) {
-		goldRemaining = goldRemaining - itemCost ;
-		document.getElementById("copper-remaining").innerHTML = goldRemaining ;
-
-		window.itemPurchaseNo += 1;
-		document.getElementById("item-purchase-no-" + window.itemPurchaseNo).innerHTML = itemName ;
-		document.getElementById("item-purchase-no-" + window.itemPurchaseNo).style.display = "block" ;
-		
-		var totalWeight = Number(document.getElementById("total-weight").innerHTML);
-		totalWeight += itemWeight;
-		document.getElementById("total-weight").innerHTML = totalWeight;
-		
-		buySpecialItem(itemName, window.itemPurchaseNo);
-		invDisplay();
-		
-		
-	} else {
-		alert("You cannot afford this item.") ;
-	}
-}
-
-var buyItemStack = function(itemQty, itemName, itemCost, itemWeight) {
-	var goldRemaining = Number( document.getElementById("copper-remaining").innerHTML ) ;
-
-	if (itemCost <= goldRemaining) {
-		goldRemaining = goldRemaining - itemCost ;
-		document.getElementById("copper-remaining").innerHTML = goldRemaining ;
-		
-		var itemQuantity = Number(document.getElementById("qty-" + itemName).innerHTML );
-				
-		itemQuantity += itemQty ;
-
-		document.getElementById("item-" + itemName).innerHTML = itemName ;
-		document.getElementById("item-" + itemName).style.display = "block" ;
-		
-		document.getElementById("qty-" + itemName).innerHTML = itemQuantity ;
-		document.getElementById("qty-" + itemName).style.display = "block" ;
-			
-		var totalWeight = Number(document.getElementById("total-weight").innerHTML);
-		totalWeight += itemWeight;
-		document.getElementById("total-weight").innerHTML = totalWeight;
-		
-		invDisplay();
-
-	} else {
-		alert("You cannot afford this item.") ;
-	}
-}
-
-
-var buySpecialItem = function(itemName, itemNumber) {
-	switch (itemName) { 
-	case "Composite Longbow":			
-		var confirmBow = prompt("Composite bows take a STR bonus. \n Input desired STR bonus. Cost is +100gp (10,000 CP) per point. \n Input a number.") ;
-		goldRemaining = Number( document.getElementById("copper-remaining").innerHTML ) ;
-		goldRemaining = goldRemaining - ( Number(confirmBow) * 10000 ) ;
-		document.getElementById("item-purchase-no-" + itemNumber ).innerHTML = itemName + " (+" + confirmBow + ")" ;		
-		document.getElementById("copper-remaining").innerHTML = goldRemaining ;
-		
-	break;		
-	case "Composite Shortbow":
-		var confirmBow = prompt("Composite bows take a STR bonus. \n Input desired STR bonus. Cost is +75gp (7,500 CP) per point. \n Input a number.") ;
-		goldRemaining = Number( document.getElementById("copper-remaining").innerHTML ) ;
-		goldRemaining = goldRemaining - ( Number(confirmBow) * 7500 ) ;
-		document.getElementById("item-purchase-no-" + itemNumber ).innerHTML = itemName + " (+" + confirmBow + ")" ;		
-		document.getElementById("copper-remaining").innerHTML = goldRemaining ;
-	break;
-	}
-}
-
-var buyWeapon = function(weaponName, itemCost, itemWeight, dmg, crit, rang, type, size, reach, hardness, hp, saves, exs) {
-	
-	var goldRemaining = Number( document.getElementById("copper-remaining").innerHTML ) ;
-
-	if (itemCost <= goldRemaining) {
-		goldRemaining = goldRemaining - itemCost ;
-		document.getElementById("copper-remaining").innerHTML = goldRemaining ;
-
-		window.itemPurchaseNo += 1;
-		document.getElementById("item-purchase-no-" + window.itemPurchaseNo).innerHTML = weaponName ;
-		document.getElementById("item-purchase-no-" + window.itemPurchaseNo).style.display = "block" ;
-		
-		var totalWeight = Number(document.getElementById("total-weight").innerHTML);
-		totalWeight += itemWeight;
-		document.getElementById("total-weight").innerHTML = totalWeight;
-	
-
-		if (window.weaponSlotOne === undefined) {
-			var confirmWa = confirm("Do you want to equip this weapon in slot one?");
-		} else if (window.weaponSlotTwo === undefined) {
-			var confirmWb = confirm("Do you want to equip this weapon in slot two?");
-		} else if (window.weaponSlotThree === undefined) {
-			var confirmWc = confirm("Do you want to equip this weapon in slot three?");
-		} else {
-			alert("You have no more open weapon slots. User-added weapon slots are not supported in this version.");
-		}
-
-		if (confirmWa) {
-			window.wSlotOne = new weaponSlot(weaponName, itemWeight, dmg, crit, rang, type, size, reach, hardness, hp, saves, exs);
-			window.weaponSlotOne = true;
-		} 
-		if (confirmWb) {
-			window.wSlotTwo = new weaponSlot(weaponName, itemWeight, dmg, crit, rang, type, size, reach, hardness, hp, saves, exs);
-			window.weaponSlotTwo = true;
-		} 
-		if (confirmWc) {
-			window.wSlotThr = new weaponSlot(weaponName, itemWeight, dmg, crit, rang, type, size, reach, hardness, hp, saves, exs);
-			window.weaponSlotThree = true;
-		} 
-
-		invDisplay();
-
-	} else {
-		alert("You cannot afford this item.") ;
-	}
-
-}
-
-
-var buyArmor = function(armorName, itemCost, itemWeight, armorBonus, maxDex, check, spFail, maxSp, hardn, hp, saves, donning, exs ) {
-
-	var goldRemaining = Number( document.getElementById("copper-remaining").innerHTML ) ;
-
-	if (itemCost <= goldRemaining) {
-		goldRemaining = goldRemaining - itemCost ;
-		document.getElementById("copper-remaining").innerHTML = goldRemaining ;
-
-		window.itemPurchaseNo += 1;
-		document.getElementById("item-purchase-no-" + window.itemPurchaseNo).innerHTML = armorName ;
-		document.getElementById("item-purchase-no-" + window.itemPurchaseNo).style.display = "block" ;
-		
-		var totalWeight = Number(document.getElementById("total-weight").innerHTML);
-		totalWeight += itemWeight;
-		document.getElementById("total-weight").innerHTML = totalWeight;
-
-		if (window.armorSlot) {
-			var changeArmor = confirm("You already have armor equipped. Replace " + window.aSlot.armorName + " with this?");
-			if (changeArmor) {
-				window.aSlot = new armor(armorName, itemWeight, armorBonus, maxDex, check, spFail, maxSp, hardn, hp, saves, donning, exs);
-			}
-		} else if (window.armorSlot === undefined) {
-			var confirmAr = confirm("Do you want to equip this armor?");
-		}
-
-		if (confirmAr) {
-			window.aSlot = new armor(armorName, itemWeight, armorBonus, maxDex, check, spFail, maxSp, hardn, hp, saves, donning);
-			window.armorSlot = true;
-		} 
-
-		if (window.armorSlot) {
-			document.getElementById("print-armor-bonus").innerHTML = window.aSlot.arBonus;
-			// Update AC on character sheet. So it ca be relied upon later for calculations during population. 
-		}	
-
-		invDisplay();
-
-	} else {
-		alert("You cannot afford this item.") ;
-	}
-}
-
-
-
-
-var buyShield = function(shieldName, itemCost, itemWeight, armorBonus, maxDex, check, spFail, maxSp, hardn, hp, saves, donning ) {
-
-	var goldRemaining = Number( document.getElementById("copper-remaining").innerHTML ) ;
-
-	if (itemCost <= goldRemaining) {
-		goldRemaining = goldRemaining - itemCost ;
-		document.getElementById("copper-remaining").innerHTML = goldRemaining ;
-
-		window.itemPurchaseNo += 1;
-		document.getElementById("item-purchase-no-" + window.itemPurchaseNo).innerHTML = shieldName ;
-		document.getElementById("item-purchase-no-" + window.itemPurchaseNo).style.display = "block" ;
-		
-		var totalWeight = Number(document.getElementById("total-weight").innerHTML);
-		totalWeight += itemWeight;
-		document.getElementById("total-weight").innerHTML = totalWeight;
-		
-		if (window.shieldSlot) {
-			var changeArmor = confirm("You already have a shield equipped. Replace " + window.sSlot.shieldName + " with this?");
-			if (changeArmor) {
-				window.sSlot = new shield(shieldName, itemWeight, armorBonus, maxDex, check, spFail, maxSp, hardn, hp, saves, donning);
-			}
-		}
-
-		if (window.shieldSlot === undefined) {
-			var confirmSh = confirm("Do you want to equip this shield?");
-		}
-
-		if (confirmSh) {
-			window.sSlot = new shield(shieldName, itemWeight, armorBonus, maxDex, check, spFail, maxSp, hardn, hp, saves, donning);
-			window.shieldSlot = true;
-		} 
-
-		if (window.sSlot) {
-			document.getElementById("print-shield-bonus").innerHTML = window.sSlot.arBonus;
-			// Update AC on character sheet.
-		}
-		
-		invDisplay();
-
-	} else {
-		alert("You cannot afford this item.") ;
-	}
-
-}
 
 
 
